b7cec009e7ac70414f95be8374651b35f9772cce
Daniel Lessenden <dless27@gmail.com>
2020-08-18 17:32:20 -0500

    Add shelter info and create shelter table

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,6 +1,7 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['Shelter 1', 'Shelter 2', 'Shelter 3']
+    #@shelters = ['Shelter 1', 'Shelter 2', 'Shelter 3']
+    @shelters = Shelter.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
 
-  #get '/shelters/:id', to: 'shelters#show'
-  #get '/shelter_1', to: 'shelter_1/show'
-  get '/shelters', to: 'shelters#index'
-  get '/shelters/new', to: 'shelters#new'
-  post '/shelters', to: 'shelters#create'
-end
+  get '/shelters/:id', to: 'shelters#show'
+  #get '/shelter_1', to: 'shelter_1#index'
+  #get '/shelters', to: 'shelters#index'
+  #get '/shelters/new', to: 'shelters#new'
+  #post '/shelters', to: 'shelters#create'
+end #need to run the db migrate command
+  #this is the example from in class exercise rails g migration CreateSongs title:string length:integer play_count:integer
