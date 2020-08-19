6202c2d95260c7dc005f1090e34938c163479297
Daniel Lessenden <dless27@gmail.com>
2020-08-18 23:02:37 -0500

    Create shelter update, delete, and creation

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -5,10 +5,46 @@ class SheltersController < ApplicationController
   end
 
   def show
-    @shelters = Shelter.find(params[:id])
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def new
   end
 
   def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+
+      })
+    shelter.save
+    redirect_to '/shelters'
   end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+    shelter.save
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
+  def destroy
+    Shelter.destroy(params[:id])
+    redirect_to '/shelters'
+  end
+
 
 end
