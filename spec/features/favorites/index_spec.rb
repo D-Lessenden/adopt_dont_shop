809d7a8bd5ab2a7d4f9419c0a38d91dffcbf04f6
Daniel Lessenden <dless27@gmail.com>
2020-08-27 14:05:48 -0500

    Add an edge case test

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'Favorites index page' do
   describe 'As a visitor' do
-    it "I see each favorited pets name and image" do
+    it "I see favorited pets name and image" do
       @shelter1 = Shelter.create!(name: "Shelter1", address: "address", city: "city", state: "state", zip: 12345)
       @pet1 = Pet.create!(image: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_34729092-e1539335371430.jpg?quality=90&strip=all&zoom=1&resize=540%2C380&ssl=1", name: "snowball", approximate_age: 5, sex: "male", shelter_id: @shelter1.id, favorite: true)
       @pet2 = Pet.create!(image: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_34729092-e1539335371430.jpg?quality=90&strip=all&zoom=1&resize=540%2C380&ssl=1", name: "fireball", approximate_age: 5, sex: "male", shelter_id: @shelter1.id)
@@ -15,4 +15,16 @@ RSpec.describe 'Favorites index page' do
 
     end
   end
+
+  it "I ONLY see favorited pets name and image" do
+    @shelter1 = Shelter.create!(name: "Shelter1", address: "address", city: "city", state: "state", zip: 12345)
+    @pet1 = Pet.create!(image: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_34729092-e1539335371430.jpg?quality=90&strip=all&zoom=1&resize=540%2C380&ssl=1", name: "snowball", approximate_age: 5, sex: "male", shelter_id: @shelter1.id, favorite: true)
+    @pet2 = Pet.create!(image: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_34729092-e1539335371430.jpg?quality=90&strip=all&zoom=1&resize=540%2C380&ssl=1", name: "fireball", approximate_age: 5, sex: "male", shelter_id: @shelter1.id)
+
+    visit '/favorites'
+
+
+    expect(page).to have_content(@pet1.name)
+    expect(page).to have_no_content(@pet2.name)
+  end 
 end
