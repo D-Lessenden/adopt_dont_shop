2e44432e6d2832ee9c4d8b7c8d1b86e4aec39e27
Daniel Lessenden <dless27@gmail.com>
2020-08-27 19:37:04 -0500

    Update to move project towards using session data

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,5 @@
 class FavoritesController < ApplicationController
+
   def index
     if params[:favorite]
       @favorite = params[:favorite]
@@ -8,20 +9,46 @@ class FavoritesController < ApplicationController
      end
   end
 
-  def update
-    pet = Pet.find(params[:pet_id])
 
-    if pet.favorite
-      pet.favorite = false
-      flash[:notice] = "Removed from favorites"
-    else
-      pet.favorite = true
-      flash[:notice] = "Added to favorites"
-    end
-    pet.save
-    redirect_to "/pets/#{pet.id}"
+  def update
+    @pet = Pet.find(params[:pet_id])
+    favorite = Favorite.new(session[:favorite])
+    favorite.add_pet(@pet)
+    session[:favorite] = favorite.fav_list
+    flash[:notice] = "Added to favorites"
+    redirect_to "/pets/#{@pet.id}"
+  end
+
+  def delete
 
   end
 
 
 end
+
+
+
+#
+# def index
+#   if params[:favorite]
+#     @favorite = params[:favorite]
+#    @pets = Pet.where favorite: params[:favorite]
+#    else
+#     @pets = Pet.all
+#    end
+# end
+#
+# def update
+#   pet = Pet.find(params[:pet_id])
+#
+#   if pet.favorite
+#     pet.favorite = false
+#     flash[:notice] = "Removed from favorites"
+#   else
+#     pet.favorite = true
+#     flash[:notice] = "Added to favorites"
+#   end
+#   pet.save
+#   redirect_to "/pets/#{pet.id}"
+#
+# end
