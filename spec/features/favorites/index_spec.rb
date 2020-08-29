b3332954da50922ae3c6f08e91702dc046dba8b4
Daniel Lessenden <dless27@gmail.com>
2020-08-28 22:34:35 -0500

    Beat the bug! favorites index page now populates correctly

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -43,15 +43,15 @@ end
     expect(page).to have_content('You have no favorited pets')
   end
 
-  it "can delete all pets on the favorite page at once" do
+  it "can delete pets on the favorite page " do
 
     @shelter1 = Shelter.create!(name: "Shelter1", address: "address", city: "city", state: "state", zip: 12345)
     @pet1 = Pet.create!(image: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_34729092-e1539335371430.jpg?quality=90&strip=all&zoom=1&resize=540%2C380&ssl=1", name: "snowball", approximate_age: 5, sex: "male", shelter_id: @shelter1.id, favorite: true)
     @pet2 = Pet.create!(image: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_34729092-e1539335371430.jpg?quality=90&strip=all&zoom=1&resize=540%2C380&ssl=1", name: "fireball", approximate_age: 5, sex: "male", shelter_id: @shelter1.id)
     visit "/pets/#{@pet1.id}"
     click_button("Add to Favorites", match: :first)
-    # visit "/pets/#{@pet2.id}"
-    # click_button("Add to Favorites", match: :first)
+    visit "/pets/#{@pet2.id}"
+    click_button("Add to Favorites", match: :first)
 
 
     visit '/favorites'
@@ -59,7 +59,7 @@ end
 
 
     expect(page).to have_no_content(@pet1.name)
-    # expect(page).to have_content(@pet2.name)
+    expect(page).to have_content(@pet2.name)
 
 
 
