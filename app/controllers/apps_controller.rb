e8984e4d4e13e0b89e3295590959ca26e9af5335
Daniel Lessenden <dless27@gmail.com>
2020-08-28 23:51:29 -0500

    Create first draft of application form

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,21 @@
 class AppsController < ApplicationController
   def index
+    if params[:App]
+      @App = params[:App]
+      @pets = Pet.where App: params[:App]
+     else
+      @pets = Pet.all
+     end
+  end
 
-  end 
+  def update
+    @pet = Pet.find(params[:pet_id])
+    app = App.new(session[:app])
+    app.add_pet(@pet)
+    session[:app] = app.fav_list
+    #flash[:notice] = "Added to favorites"
+    #redirect_to "/pets/#{@pet.id}"
+  end
+
+
 end
