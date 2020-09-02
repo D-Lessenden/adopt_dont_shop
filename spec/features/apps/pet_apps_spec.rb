80b8be1b31e3ba36b743cd5b888b9805e9edfb58
Daniel Lessenden <dless27@gmail.com>
2020-09-02 18:19:43 -0500

    Final touches

--- spec/features/apps/pet_apps_spec.rb
+++ spec/features/apps/pet_apps_spec.rb
@@ -53,4 +53,17 @@ RSpec.describe 'ApplicationPets index page' do
     click_on 'Applications'
     expect(page).to have_content("No applications for this pet")
   end
+
+  it "has a message if there are no applications" do
+    @shelter1 = Shelter.create!(name: "Shelter1", address: "address", city: "city", state: "state", zip: 12345)
+    @pet1 = Pet.create!(image: "https://i0.wp.com/metro.co.uk/wp-content/uploads/2018/10/sei_34729092-e1539335371430.jpg?quality=90&strip=all&zoom=1&resize=540%2C380&ssl=1", name: "snowball", approximate_age: 5, sex: "male", shelter_id: @shelter1.id, description: "qwerty")
+    @app1 = App.create!(name: "app1", address: "121 Silly Ave", city: "Sillytown", state: "CO", zip: 80022, phone_number: 7207202, description: "we love dogs", pet_id: @pet1.id)
+    ApplicationPet.create!(app_id: @app1.id, pet_id: @pet1.id)
+
+    visit "/apps/pet_apps/#{@app1.id}"
+
+  end
+
+
+
 end
