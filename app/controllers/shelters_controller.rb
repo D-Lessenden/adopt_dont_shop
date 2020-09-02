b83ae4a8584ee91605d1f131f96e2e76cd3b18f7
Daniel Lessenden <dless27@gmail.com>
2020-09-02 16:55:42 -0500

    Add flash messages to shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -46,9 +46,14 @@ class SheltersController < ApplicationController
     if shelter.save
       redirect_to "/shelters/#{shelter.id}"
     else
-      flash[:alert] = "All feilds are required"
-      redirect_to "/shelters/#{shelter.id}/edit"
+          flash[:alert_name] = "Name is a required field" if shelter.name.empty?
+          flash[:alert_address] = "Address is a required field" if shelter.address.empty?
+          flash[:alert_city] = "City is a required field" if shelter.city.empty?
+          flash[:alert_state] = "State is a required field" if shelter.state.empty?
+          flash[:alert_zip] = "Zip is a required field" if shelter.zip.nil?
+          redirect_to "/shelters/#{shelter.id}/edit"
     end
+    
   end
 
   def destroy
